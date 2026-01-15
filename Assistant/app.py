1783b65a818b367509817adc86a3dde933d1cf30
varad <varadpk20@student.sfit.ac.in>
2026-01-15 21:50:28 +0530

    local assistant

---
+++ Assistant/app.py
@@ -0,0 +1,9 @@
+from langchain_ollama import OllamaLLM
+
+llm = OllamaLLM(
+    model="mistral:7b-instruct",
+    base_url="http://localhost:11434",
+)
+
+if __name__ == "__main__":
+    print(llm.invoke("What is weather in mumbai"))
